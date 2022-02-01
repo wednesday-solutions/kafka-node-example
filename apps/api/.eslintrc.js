e5ebc34ce842c92635d871c445e573caec07c59c
tousifws <82563595+tousifws@users.noreply.github.com>
2022-02-01 17:52:30 +0530

    fix: eslint

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,8 +1,23 @@
 module.exports = {
-  ...require('config/eslint-preset'),
-  parserOptions: {
-    root: true,
-    tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json']
-  }
-}
+    ...require("config/eslint-preset"),
+    env: {
+        // Your environments (which contains several predefined global variables)
+        // browser: true,
+        node: true,
+        // mocha: true,
+        jest: true,
+    },
+    globals: {
+        // Your global variables (setting to false means it's not allowed to be reassigned)
+        //
+        // myGlobal: false
+    },
+    rules: {
+        // Customize your rules
+    },
+    parserOptions: {
+        root: true,
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+    },
+};
--- packages/config/eslint-preset.js
+++ packages/config/eslint-preset.js
@@ -1,21 +1,6 @@
 module.exports = {
-    extends: ["alloy", "alloy/typescript", "alloy/react", "plugin:unicorn/recommended"],
+    extends: ["alloy", "alloy/typescript", "alloy/react"],
     ignorePatterns: ["**/migrations/*.ts", "dist/*"],
-    env: {
-        // Your environments (which contains several predefined global variables)
-        browser: true,
-        node: true,
-        // mocha: true,
-        jest: true,
-    },
-    globals: {
-        // Your global variables (setting to false means it's not allowed to be reassigned)
-        //
-        // myGlobal: false
-    },
-    rules: {
-        // Customize your rules
-    },
     overrides: [
         {
             files: ["*.resolver.ts"],
