a873be84e7e8fb91e4b8810cc98b8b818cfbaa2d
tousifws <82563595+tousifws@users.noreply.github.com>
2022-02-11 16:55:10 +0530

    chore: updating author name

--- packages/ui-components/src/components/Button.tsx
+++ packages/ui-components/src/components/Button.tsx
@@ -1,9 +1,5 @@
 import react from "react";
 
 export default function Button() {
-  return (
-    <button className="p-1 px-2 rounded-lg bg-lime-300">
-      Hit me baby, one more time
-    </button>
-  );
+    return <button className="p-1 px-2 rounded-lg bg-lime-300">Click Me</button>;
 }
