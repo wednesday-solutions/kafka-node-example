9b3c2ae66c0dc4c325d42f8075e5d9f700af88a3
Tousif <82563595+tousifws@users.noreply.github.com>
2022-04-27 13:00:35 +0530

    test: fix jest (#3)

--- apps/producer_server/jest.config.js
+++ apps/producer_server/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/$1",
     },
+    modulePathIgnorePatterns: ["<rootDir>/dist/"],
     setupFilesAfterEnv: ["jest-extended/all"],
     globalSetup: "./test/global-setup.js",
     globalTeardown: "./test/global-teardown.js",
